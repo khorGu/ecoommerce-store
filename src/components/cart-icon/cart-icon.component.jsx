e9f440bc7c6b4511db096f57e2b24b11aa7c1f5e
khorGu <bekaxorgu31@gmail.com>
2022-04-28 21:54:50 +0400

    completed cart Menu

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,7 +4,7 @@ import { CartContext } from "../../contexts/cart.context";
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
   const toogleCart = () => {
     setIsCartOpen(!isCartOpen);
@@ -12,7 +12,7 @@ const CartIcon = () => {
   return (
     <div className="cart-icon-container" onClick={toogleCart}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
